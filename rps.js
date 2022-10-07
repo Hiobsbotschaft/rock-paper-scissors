a897a74034a552e7dd1445e4a9d1ea939e7b95a3
Sarah Keller <kellersarah2@gmail.com>
2022-10-07 15:50:23 +0200

    final product

--- rps.js
+++ rps.js
@@ -1,39 +1,81 @@
-const choices = [ 'rock','paper','scissors'];
+let computerwins = 0
+let playerwins = 0
+const choices = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice () { 
+function getComputerChoice() {
 
-    return choices[Math.floor(Math.random()*choices.length)];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let computerSelection = getComputerChoice ();
+function getPlayersChoice() {
 
-let playerSelection =prompt("'Enter your Choice!'").toLowerCase();
-    
-
-function playRound (computerSelection,playerSelection) {
-
-    if (computerSelection =='paper' && playerSelection =='rock') {
-        return ('"You Lose! Paper beats Rock"')
-    }
-    else if (computerSelection =='rock' && playerSelection =='scissors') {
-        return ('"You Lose! Rock beats Scissors"')
-    }
-    else if (computerSelection =='paper' && playerSelection =='scissors') {
-        return ('"You Win! Scissors beats Paper"')
-    }
-    else if (computerSelection =='scissors' && playerSelection =='rock') {
-        return ('"You Win! Rock beats Scissors"')
-    }
-    else if (computerSelection =='rock' && playerSelection =='paper') {
-        return ('"You Win! Paper beats Rock"')
-    }
-    else if (computerSelection =='scissors' && playerSelection =='paper') {
-        return ('"You Lose! Scissors beats Paper"')
-    }
-    else {
-        return('"Its a Draw!"')
-    }
+  const choice= prompt("'Enter your Choice!'").toLowerCase();
+  if (!choices.includes(choice)) {
+   computerwins++; 
+   console.log('You suck !')
+  }
+  
+  return choice;
 }
 
 
-console.log(playRound (computerSelection,playerSelection));
+function compareSelection(computerSelectionArg, playerSelectionArg) {
+
+  if (computerSelectionArg == 'paper' && playerSelectionArg == 'rock') {
+    //computerwins = computerwins + 1
+    computerwins++;
+    return ('"You Lose! Paper beats Rock"')
+  }
+  else if (computerSelectionArg == 'rock' && playerSelectionArg == 'scissors') {
+    computerwins++;
+    return ('"You Lose! Rock beats Scissors"')
+  }
+  else if (computerSelectionArg == 'paper' && playerSelectionArg == 'scissors') {
+    playerwins++;
+    return ('"You Win! Scissors beats Paper"')
+  }
+  else if (computerSelectionArg == 'scissors' && playerSelectionArg == 'rock') {
+    playerwins++;
+    return ('"You Win! Rock beats Scissors"')
+  }
+  else if (computerSelectionArg == 'rock' && playerSelectionArg == 'paper') {
+    playerwins++;
+    return ('"You Win! Paper beats Rock"')
+  }
+  else if (computerSelectionArg == 'scissors' && playerSelectionArg == 'paper') {
+    computerwins++;
+    return ('"You Lose! Scissors beats Paper"')
+  }
+  else if (computerSelectionArg && !choices.includes(playerSelectionArg)) {
+    return ('You just gifted the computer a win ! Congratz Loser!')
+  }
+  else {
+    return ('"Its a Draw!"')
+  }
+}
+
+
+function playRound() {
+  const computerSelection = getComputerChoice();
+  const playerSelection = getPlayersChoice();
+  console.log(compareSelection (computerSelection,playerSelection));
+}
+
+for (let i=0;i<5;i++) {
+  playRound();
+}
+
+function checkWinner () {
+  if (playerwins > computerwins) {
+    return ('"You Win! Yaaaay"')
+  }
+  else if (playerwins < computerwins){
+    return ('"BOOOO You lose ! LOSER "')
+  }
+  else {
+    return ('"Oh well....a draw !"')
+  }
+}
+
+
+console.log(checkWinner());
---
+++ rpsold.js
@@ -0,0 +1,38 @@
+const choices = [ 'rock','paper','scissors'];
+
+function getComputerChoice () { 
+
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+let computerSelection = getComputerChoice ();
+
+let playerSelection =prompt("'Enter your Choice!'").toLowerCase();
+    
+function game (){
+    
+        function playRound (computerSelection,playerSelection) {
+
+        if (computerSelection =='paper' && playerSelection =='rock') {
+            return ('"You Lose! Paper beats Rock"')
+        }
+        else if (computerSelection =='rock' && playerSelection =='scissors') {
+            return ('"You Lose! Rock beats Scissors"')
+        }
+        else if (computerSelection =='paper' && playerSelection =='scissors') {
+            return ('"You Win! Scissors beats Paper"')
+        }
+        else if (computerSelection =='scissors' && playerSelection =='rock') {
+            return ('"You Win! Rock beats Scissors"')
+        }
+        else if (computerSelection =='rock' && playerSelection =='paper') {
+            return ('"You Win! Paper beats Rock"')
+        }
+        else if (computerSelection =='scissors' && playerSelection =='paper') {
+            return ('"You Lose! Scissors beats Paper"')
+        }
+        else {
+            return('"Its a Draw!"')
+        }
+}
+}
