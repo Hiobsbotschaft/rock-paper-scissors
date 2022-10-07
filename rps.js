9951971a1104a119ba7263b88801c987ac1ebfb5
Sarah Keller <kellersarah2@gmail.com>
2022-10-07 16:02:49 +0200

    code cleanup

--- rps.js
+++ rps.js
@@ -3,13 +3,11 @@ let playerwins = 0
 const choices = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getPlayersChoice() {
-
-  const choice= prompt("'Enter your Choice!'").toLowerCase();
+  const choice= prompt('Enter your Choice!').toLowerCase();
   if (!choices.includes(choice)) {
    computerwins++; 
    console.log('You suck !')
@@ -18,42 +16,39 @@ function getPlayersChoice() {
   return choice;
 }
 
-
 function compareSelection(computerSelectionArg, playerSelectionArg) {
-
   if (computerSelectionArg == 'paper' && playerSelectionArg == 'rock') {
     //computerwins = computerwins + 1
     computerwins++;
-    return ('"You Lose! Paper beats Rock"')
+    return ('You Lose! Paper beats Rock')
   }
   else if (computerSelectionArg == 'rock' && playerSelectionArg == 'scissors') {
     computerwins++;
-    return ('"You Lose! Rock beats Scissors"')
+    return ('You Lose! Rock beats Scissors')
   }
   else if (computerSelectionArg == 'paper' && playerSelectionArg == 'scissors') {
     playerwins++;
-    return ('"You Win! Scissors beats Paper"')
+    return ('You Win! Scissors beats Paper')
   }
   else if (computerSelectionArg == 'scissors' && playerSelectionArg == 'rock') {
     playerwins++;
-    return ('"You Win! Rock beats Scissors"')
+    return ('You Win! Rock beats Scissors')
   }
   else if (computerSelectionArg == 'rock' && playerSelectionArg == 'paper') {
     playerwins++;
-    return ('"You Win! Paper beats Rock"')
+    return ('You Win! Paper beats Rock')
   }
   else if (computerSelectionArg == 'scissors' && playerSelectionArg == 'paper') {
     computerwins++;
-    return ('"You Lose! Scissors beats Paper"')
+    return ('You Lose! Scissors beats Paper')
   }
   else if (computerSelectionArg && !choices.includes(playerSelectionArg)) {
     return ('You just gifted the computer a win ! Congratz Loser!')
   }
   else {
-    return ('"Its a Draw!"')
+    return ('Its a Draw!')
   }
 }
-
 
 function playRound() {
   const computerSelection = getComputerChoice();
@@ -61,21 +56,24 @@ function playRound() {
   console.log(compareSelection (computerSelection,playerSelection));
 }
 
-for (let i=0;i<5;i++) {
-  playRound();
-}
 
 function checkWinner () {
   if (playerwins > computerwins) {
-    return ('"You Win! Yaaaay"')
+    return ('You Win! Yaaaay')
   }
   else if (playerwins < computerwins){
-    return ('"BOOOO You lose ! LOSER "')
+    return ('BOOOO You lose ! LOSER ')
   }
   else {
-    return ('"Oh well....a draw !"')
+    return ('Oh well....a draw !')
   }
 }
 
+function playGame (){
+  for (let i=0;i<5;i++) {
+  playRound();
+  }
+  console.log(checkWinner());
+}
 
-console.log(checkWinner());
+playGame();
